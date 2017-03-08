6f7d35a12fb4e47e4471a32aeeef5990224d9bb5
Benjamin Matson <benjaminmatson@gmail.com>
2017-03-08 11:18:26 -0700

    login & logut working

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,4 @@
 # instance/config.py
 
-SECRET_KEY = '' #fake key p9Bv<3Eid9%$i01
+SECRET_KEY = 'im super serial' #fake key p9Bv<3Eid9%$i01
 SQLALCHEMY_DATABASE_URI = 'mysql://py_admin:py314@localhost/dreamteam_db'
